7ad67724cd004542d3070394b4a12d1a58f4e6ad
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-05-14 11:13:56 -0300

    Spring Boot project with docker-compose and DB

---
+++ src/test/kotlin/com/ingsis/codeprocessing/CodeProcessingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis.codeprocessing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeProcessingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
