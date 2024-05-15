12016ca11c696f43669bb87b58e61b9463b8d7af
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-05-15 15:26:09 -0300

    Ktlint Linter and Formatter

--- src/main/kotlin/com/ingsis/codeprocessing/CodeProcessingApplication.kt
+++ src/main/kotlin/com/ingsis/codeprocessing/CodeProcessingApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class CodeProcessingApplication
 
 fun main(args: Array<String>) {
-	runApplication<CodeProcessingApplication>(*args)
+    runApplication<CodeProcessingApplication>(*args)
 }
--- src/test/kotlin/com/ingsis/codeprocessing/CodeProcessingApplicationTests.kt
+++ src/test/kotlin/com/ingsis/codeprocessing/CodeProcessingApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CodeProcessingApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
